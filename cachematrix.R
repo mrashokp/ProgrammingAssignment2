6c3ad96a4da22b91b9ff36339ebc48fda7821a7b
Ashok Patil <mrashokp@gmail.com>
2015-07-23 13:39:45 +0530

    cachematrix.R modified

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## These functions will calculate an inverse of a matrix if it is not already stored in the cache. 
 
 ## This function will create a list of four functions:
-#1. 'setmatrix' will create the function to set the value of matrix and inverse(inv for short)
+#1. 'setmatrix' will create the function to set the value of matrix and inverse(inv for short) in cache.
 #2. 'getmatrix' will create a function to get the matrix 
 #3. 'setinv' will create a function to set/calculate the inverse of this matrix
 #4. 'getinv' will create a function to get the value of inverse.
@@ -22,7 +22,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function takes the above function as it's argument and checks whether the inverse value is stored in cache. If stored, gets the cached value and displays; if not, calculates it and displays.
+## This function takes the above function as it's argument and checks whether the inverse value is stored in cache. 
+#If stored, gets the cached value and displays; if not, calculates it and displays.
 
 cacheSolve <- function(x=makeCacheMatrix, ...) {
         inv<-x$getinv()
